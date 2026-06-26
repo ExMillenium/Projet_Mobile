8ad16a8ce5177476aff7a20646c857b2ca65a612
Millenium <thomas.rawidjan@gmail.com>
2026-06-26 07:40:46 -0400

    Changes

--- GEC_API/src/Controllers/StudentController.php
+++ GEC_API/src/Controllers/StudentController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Controllers;
 
 use Managers\StudentManager;
@@ -11,15 +10,36 @@ class StudentController
     public function __construct($pdo)
     {
         $this->manager = new StudentManager($pdo);
+        header('Content-Type: application/json');
     }
 
     public function getAll()
     {
-        echo json_encode($this->manager->getAll());
+        try {
+            $students = $this->manager->getAll();
+            http_response_code(200);
+            echo json_encode($students);
+        } catch (\Exception $e) {
+            http_response_code(500);
+            echo json_encode(['error' => $e->getMessage()]);
+        }
     }
 
     public function getByClass($params)
     {
-        echo json_encode($this->manager->getByClass($params['id']));
+        if (!isset($params['id'])) {
+            http_response_code(400);
+            echo json_encode(['error' => 'Missing class ID']);
+            return;
+        }
+
+        try {
+            $students = $this->manager->getByClass($params['id']);
+            http_response_code(200);
+            echo json_encode($students);
+        } catch (\Exception $e) {
+            http_response_code(500);
+            echo json_encode(['error' => $e->getMessage()]);
+        }
     }
 }
--- GEC_API/src/Core/Router.php
+++ GEC_API/src/Core/Router.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Core;
 
 class Router
@@ -11,16 +10,43 @@ class Router
         $this->routes['GET'][$path] = $callback;
     }
 
+    public function post(string $path, callable $callback)
+    {
+        $this->routes['POST'][$path] = $callback;
+    }
+
+    public function put(string $path, callable $callback)
+    {
+        $this->routes['PUT'][$path] = $callback;
+    }
+
+    public function delete(string $path, callable $callback)
+    {
+        $this->routes['DELETE'][$path] = $callback;
+    }
+
     public function resolve(string $method, string $uri)
     {
+        header('Content-Type: application/json');
+
         $uri = explode("?", $uri)[0];
 
+        if (!isset($this->routes[$method])) {
+            http_response_code(405);
+            echo json_encode(["error" => "Method not allowed"]);
+            return;
+        }
+
         foreach ($this->routes[$method] as $route => $callback) {
+
             $pattern = "@^" . preg_replace('/\{(\w+)\}/', '(?P<\1>[\w\-]+)', $route) . "$@";
 
-
             if (preg_match($pattern, $uri, $matches)) {
-                return $callback($matches);
+
+                // Nettoyage : on retire les index numériques
+                $params = array_filter($matches, fn($k) => !is_int($k), ARRAY_FILTER_USE_KEY);
+
+                return $callback($params);
             }
         }
 
