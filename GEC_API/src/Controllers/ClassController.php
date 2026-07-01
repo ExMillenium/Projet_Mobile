c610004571cc2f5ce68c5c94a2c3f48f02f1537c
Millenium <thomas.rawidjan@gmail.com>
2026-07-01 10:31:04 -0400

    Update ClassController.php

--- GEC_API/src/Controllers/ClassController.php
+++ GEC_API/src/Controllers/ClassController.php
@@ -15,6 +15,13 @@ class ClassController
 
     public function getAll()
     {
-        echo json_encode($this->manager->getAll());
-    }
+        try {
+            $students = $this->manager->getAll();
+            http_response_code(200);
+            echo json_encode($students);
+        } catch (\Exception $e) {
+            http_response_code(500);
+            echo json_encode(['error' => $e->getMessage()]);
+        }
+    }   
 }
